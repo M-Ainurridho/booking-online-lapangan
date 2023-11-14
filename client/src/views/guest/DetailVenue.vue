7d054bfcd216bd07816adc50da083b9e55f38bc2
M-Ainurridho <m.ainurridho11@gmail.com>
2023-11-14 17:25:12 +0700

    static data waktu booking

--- client/src/views/guest/DetailVenue.vue
+++ client/src/views/guest/DetailVenue.vue
@@ -107,23 +107,59 @@
                               <p class="m-0 fs-6"><i class="bx bx-map-pin me-1"></i> Indoor</p>
                               <p class="m-0 fs-6"><i class="bx bx-water me-1"></i> Lantai</p>
                            </div>
-                           <button class="border bg-navy text-white fw-semibold py-2 px-5 mt-3 rounded-3 d-flex align-items-center column-gap-2">7 Lapangan Tersedia <i class="bx bx-up-arrow-circle fs-4"></i></button>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="col-12">
-                     <div class="row px-3 py-4 border-bottom detail-lapangan">
-                        <div class="col-4">
-                           <img src="../../assets/images/venue/badminton.jpg" alt="gambar" class="w-100 object-fit-cover rounded-3" />
-                        </div>
-                        <div class="col-8 border-start">
-                           <h5>Lap 2</h5>
-                           <div class="spek-lapangan">
-                              <p class="m-0 fs-6"><i class="bx bx-basketball me-1"></i> Basketball</p>
-                              <p class="m-0 fs-6"><i class="bx bx-map-pin me-1"></i> Indoor</p>
-                              <p class="m-0 fs-6"><i class="bx bx-water me-1"></i> Lantai</p>
+                           <button class="border bg-navy text-white fw-semibold px-3 py-2 mt-3 rounded-3 d-flex align-items-center column-gap-2" @click="dropdown = !dropdown">
+                              8 Lapangan Tersedia <i class="bx fs-4" :class="dropdown ? 'bx-down-arrow-circle' : 'bx-up-arrow-circle'"></i>
+                           </button>
+                           <div v-if="dropdown" class="waktu-booking mt-4 pt-3 border-top">
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
+                              <div class="card">
+                                 <div class="card-body text-center">
+                                    <div class="card-title m-0 fw-semibold">17:00 - 18:00</div>
+                                    <div class="card-text text-opacity-50" style="font-size: 14px">Rp30.000</div>
+                                 </div>
+                              </div>
                            </div>
-                           <button class="border bg-navy text-white fw-semibold py-2 px-5 mt-3 rounded-3 d-flex align-items-center column-gap-2">7 Lapangan Tersedia <i class="bx bx-up-arrow-circle fs-4"></i></button>
                         </div>
                      </div>
                   </div>
@@ -207,6 +243,20 @@ sub {
 
 .spek-lapangan p {
    margin-bottom: -12px !important;
+}
+
+.spek-lapangan button {
+   width: 30%;
+}
+
+.waktu-booking {
+   display: grid;
+   grid-template-columns: repeat(4, 1fr);
+   gap: 10px;
+}
+
+.waktu-booking .card {
+   cursor: pointer;
 }
 
 @media screen and (max-width: 992px) {
